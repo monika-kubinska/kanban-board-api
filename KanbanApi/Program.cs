e0969139fe5f040e511e8106ba311a3056f2efd7
monika-kubinska <monika.rzepecka@capgemini.com>
2026-06-21 14:48:32 +0200

    Refactor code structure for improved readability and maintainability

--- KanbanApi/Program.cs
+++ KanbanApi/Program.cs
@@ -3,13 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseNpgsql("Host=localhost;Port=5432;Database=kanban_db;Username=kanban;Password=kanban"));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
